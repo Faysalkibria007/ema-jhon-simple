2075f608f83b5167bd1793c59dcf87c816b1b7d0
azad996 <alauddinj786@gmail.com>
2023-03-29 16:17:50 +0600

    first commit

--- src/Component/Shop/Shop.jsx
+++ src/Component/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -14,14 +15,27 @@ fetch('products.json')
 .then(data => setProducts(data))
 }, [] )
 
+useEffect(()=>{
+    const storedCart = getShoppingCart()
+
+    const savedCart = []
+for (const id in storedCart){
+
+    const addedProduct = products.find(product => product.id === id)
+if (addedProduct){
+    const quantity = storedCart[id]
+    addedProduct.quantity = quantity
+    savedCart.push(addedProduct)
+}
+    
+}
+setCart(savedCart)
+}, [products])
 const handleAddToCart = (product) => {
     const newCart = [...cart, product]
     setCart(newCart)
-    
-
-
+    addToDb(product.id)
         }
-
 
     return (
         <div className="shop-container">
